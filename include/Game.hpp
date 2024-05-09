ca0d89ec554366b3a78b2e39770b21ec5b80fd6a
Mario <33102086+mzcpp@users.noreply.github.com>
2024-05-09 19:30:43 +0200

    Add files via upload

---
+++ include/Game.hpp
@@ -0,0 +1,65 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include "QuadTree.hpp"
+
+#include <SDL2/SDL.h>
+
+#include <memory>
+#include <list>
+
+template <typename T>
+class Shape;
+
+template <typename T>
+class Rect;
+
+class CircleTexture;
+
+typedef Rect<float> QtItemType;
+
+class Game
+{
+private:
+	bool initialized_;
+	bool running_;
+
+	SDL_Point mouse_pos_;
+	bool mouse_moved_;
+	bool left_shift_pressed_;
+	bool debug_areas_;
+	std::unique_ptr<QuadTree<QtItemType>> qt_;
+	std::unique_ptr<Shape<float>> shape_area_;
+	std::list<QuadTree<QtItemType>::QuadTreeItemListIt> found_items_;
+
+	float circle_r_;
+	float rect_side_;
+
+	bool searching_;
+	bool removing_;
+	
+	std::list<std::list<QtItemType>::iterator> found_;
+	std::unique_ptr<CircleTexture> circle_texture_;
+
+	SDL_Window* window_;
+	SDL_Renderer* renderer_;
+
+public:
+	Game();
+
+	~Game();
+
+	bool Initialize();
+
+	void Finalize();
+
+	void Run();
+
+	void HandleEvents();
+	
+	void Tick();
+	
+	void Render();
+};
+
+#endif
